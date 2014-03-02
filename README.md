bb8f4375118d61cfbf2ca0fba5511928fe351d93
verdverm <verdverm@gmail.com>
2014-03-02 03:53:22 -0500

    added onmousemove, added rate limit to AddHandler

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ mightymouse-js
 
 ![Here I come to save the daaaaay!](https://raw.github.com/verdverm/mightymouse-js/master/MightyMouseandGirl.gif)
 
-mightymouse-js makes working with mouse traps a snap! just watch your tail.
+mightymouse-js makes working with mouse traps a snap! Just watch that tail!
 
 ```JavaScript
-mightymouse.AddHandler("body", "lbtn clk", function(e) {
+mightymouse.AddHandler("body", "lbtn click", function(e) {
     console.log("left button clicked!")
 });
 ```
@@ -26,37 +26,48 @@ the API is as follows
 
 ```JavaScript
 buttons   = ["lbtn", "mbtn", "rbtn"];
-events    = ["clk", "dbl", "down", "up", "scroll"];
+events    = ["click", "double", "down", "up", "move", "scroll"];
 modifiers = ["alt", "ctrl", "shift", "meta"];
 prevent?  = ["prevent"];
 
-mightmouse.AddHandler(JQuerySelector, MouseString, Handler);
+mightmouse.AddHandler(JQuerySelector, MouseString, Handler, Limit);
 ```
 
-- You can only use one event and one button. 
-- 'scroll' ignores buttons.
-- 'rbtn' ignores events (oncontextmenu).
-- You can use as many modifiers as you like, but mightymouse tests that those you didn't include, aren't pressed down as well.
+You can use one event, one button, and as many modifiers as you like. 
+mightymouse-js checks that those modifiers you didn't include, are not pressed down.
+Limit is optional and specifies how often the handler can be invoked.
+
+caveats:
+
+- `move` does not require buttons
+- `scroll` ignores buttons.
+- `rbtn` ignores events (oncontextmenu).
+- `prevent` inserts a call to `preventDefault()` 
 
 
 Examples
 --------------
 
+Call `mightmouse.TestMices() to run the tests and add sample handlers
+
 ```JavaScript
-mightymouse.AddHandler("body", "lbtn clk", function(e) {
-    console.log("someone help me!")
+mightymouse.AddHandler("body", "rbtn prevent", function(e) {
+    console.log("right mouse prevented!");
 });
-mightymouse.AddHandler("body", "lbtn clk ctrl", function(e) {
+mightymouse.AddHandler("body", "lbtn click ctrl", function(e) {
     console.log("mighty mouse to the rescue!")
 });
-mightymouse.AddHandler(".cat", "lbtn clk ctrl shift", function(e) {
+mightymouse.AddHandler("body", "lbtn click shift", function(e) {
+    console.log("mighty mouse to the rescue2!")
+});
+mightymouse.AddHandler("body", "lbtn click ctrl shift", function(e) {
     console.log("UP UP AND AWAY!");
 });
 
-// stop the context menu
-mightymouse.AddHandler("#mydiv", "rbtn prevent", function(e) {
-    console.log("right mouse prevented!");
-});
+var rateLimit = 200;
+mightymouse.AddHandler("body", "move", function(e) {
+     console.log("MM is @: " + e.clientX + "," + e.clientY)
+}, rateLimit);
 ```
 
 Issues
--- mightymouse.js
+++ mightymouse.js
@@ -1,283 +1,324 @@
 // mightymouse.js
+// copyright 2014
+// Tony Worm  verdverm@gmail.com
 
-var mightymouse = {}
 
-// $({
-var b_buttn = ["lbtn", "mbtn", "rbtn"];
-var b_event = ["clk", "dbl", "down", "up", "scroll"];
-var b_extra = ["alt", "ctrl", "shift", "meta"];
+var mightymouse = (function($) {
 
-mightymouse.AddHandler = function(jqry_sel, event_str, handler) {
-    $(jqry_sel).addClass("mm_click");
-    var mm_handle = parseEvent(event_str, handler);
-    $(jqry_sel).on(mm_handle.type, mm_handle.handler);
-}
+    var MM = {};
 
-function parseEvent(str, hdl) {
+    var b_buttn = ["lbtn", "mbtn", "rbtn"];
+    var b_event = ["click", "double", "down", "up", "move", "scroll"];
+    var b_extra = ["alt", "ctrl", "shift", "meta"];
 
-    // first parse the string
-    var params = {
-        prevent: false,
-        btn: "",
-        evt: "",
-        xtrs: [],
-    }
 
-    var flds = str.split(" ")
-    var empty = true;
-    for (var i = 0; i < flds.length; i++) {
-        var f = flds[i];
-        if (f.length < 1) {
-            continue;
+    MM.AddHandler = function(jqry_sel, event_str, handler, limit) {
+        $(jqry_sel).addClass("mm_click");
+        var mm_handle = parseEvent(event_str, handler, limit);
+        if (mm_handle == null) {
+            console.log("mightmouse-js: Error adding handler")
         }
-        if (empty) {
-            empty = false;
+
+        $(jqry_sel).on(mm_handle.type, mm_handle.handler);
+    }
+
+    function parseEvent(str, hdl, lmt) {
+
+        // first parse the string
+        var params = {
+            prevent: false,
+            btn: "",
+            evt: "",
+            xtrs: [],
         }
-        switch (f) {
-            case "prevent":
-                params.prevent = true;
-                break;
-            case "lbtn":
-            case "mbtn":
-            case "rbtn":
-                if (params.btn != "") {
-                    console.log("error parsing'" + str + "': more than one button specified");
-                    return null;
-                }
-                params.btn = f;
-                break;
 
-            case "clk":
-            case "dbl":
-            case "down":
-            case "up":
-            case "scroll":
-                if (params.evt != "") {
-                    console.log("error parsing'" + str + "': more than one event specified");
-                    return null;
-                }
-                params.evt = f;
-                break;
-
-            case "alt":
-            case "ctrl":
-            case "shift":
-            case "meta":
-                params.xtrs.push(f);
-                break;
-
-            default:
-                console.log("error parsing'" + str + "': unknown input'" + f + "'");
-                return null;
-        }
-    }
-
-    if (empty) {
-        console.log("error parsing'" + str + "': empty input'");
-        return null
-    }
-
-
-    // then build the handler
-    var type_str = "";
-    var handle = hdl;
-    var handleB = function(e) {
-        console.log("handleB")
-    };
-    var handleP = function(e) {
-        console.log("handleP")
-    };
-    var handleX = function(e) {
-        console.log("handleX")
-    };
-
-
-    // special case for rbtn, which ignores the event field (why is the right button being discriminated against)
-    if (params.btn == "rbtn") {
-        type_str = "contextmenu";
-        handleB = handle;
-    }
-    // special case for scroll, which ignores the button field
-    else if (params.evt == "scroll") {
-        type_str = "scroll";
-        handleB = handle;
-    }
-    // default case needs to wrap the current handler for button number
-    else {
-        // type str
-        switch (params.evt) {
-            case "clk":
-                type_str = "click";
-                break;
-            case "dbl":
-                type_str = "dblclick";
-                break;
-            case "down":
-                type_str = "mousedown";
-                break;
-            case "up":
-                type_str = "mouseup";
-                break;
-            default:
-                console.log("Shouldn't get here (params.evt)")
-                return null;
-        }
-        // btn determination
-        switch (params.btn) {
-            case "lbtn":
-                handleB = function(e) {
-                    if (e.button == 0) {
-                        return handle(e)
+        var flds = str.split(" ")
+        var empty = true;
+        for (var i = 0; i < flds.length; i++) {
+            var f = flds[i];
+            if (f.length < 1) {
+                continue;
+            }
+            if (empty) {
+                empty = false;
+            }
+            switch (f) {
+                case "prevent":
+                    params.prevent = true;
+                    break;
+                case "lbtn":
+                case "mbtn":
+                case "rbtn":
+                    if (params.btn != "") {
+                        console.log("error parsing'" + str + "': more than one button specified");
+                        return null;
                     }
-                }
-                break;
-            case "mbtn":
-                handleB = function(e) {
-                    if (e.button == 1) {
-                        return handle(e)
+                    params.btn = f;
+                    break;
+
+                case "click":
+                case "double":
+                case "down":
+                case "up":
+                case "move":
+                case "scroll":
+                    if (params.evt != "") {
+                        console.log("error parsing'" + str + "': more than one event specified");
+                        return null;
                     }
-                }
-                break;
-            default:
-                console.log("Shouldn't get here (params.btn)")
-                return null;
-        }
+                    params.evt = f;
+                    break;
 
-
-
-    }
-    var bitvec = [false, false, false, false];
-    // add meta key handler
-    if (params.xtrs.length > 0) {
-        var does = params.xtrs;
-        for (var i = 0; i < does.length; i++) {
-            switch (does[i]) {
                 case "alt":
-                    bitvec[0] = true;
-                    break;
                 case "ctrl":
-                    bitvec[1] = true;
-                    break;
-                case "meta":
-                    bitvec[2] = true;
-                    break;
                 case "shift":
-                    bitvec[3] = true;
+                case "meta":
+                    params.xtrs.push(f);
+                    break;
+
+                default:
+                    console.log("error parsing'" + str + "': unknown input'" + f + "'");
+                    return null;
+            }
+        }
+
+        if (empty) {
+            console.log("error parsing'" + str + "': empty input'");
+            return null
+        }
+
+
+        // then build the handler
+        var type_str = "";
+        var handle = hdl;
+        var handleB = function(e) {
+            console.log("handleB")
+        };
+        var handleP = function(e) {
+            console.log("handleP")
+        };
+        var handleX = function(e) {
+            console.log("handleX")
+        };
+
+
+        // special case for rbtn, which ignores the event field (why is the right button being discriminated against)
+        if (params.btn == "rbtn") {
+            type_str = "contextmenu";
+            handleB = handle;
+        }
+        // special case for scroll, which ignores the button field
+        else if (params.evt == "scroll") {
+            type_str = "scroll";
+            handleB = handle;
+        }
+        // default case needs to wrap the current handler for button number
+        else {
+            // type str
+            switch (params.evt) {
+                case "click":
+                    type_str = "click";
+                    break;
+                case "double":
+                    type_str = "dblclick";
+                    break;
+                case "down":
+                    type_str = "mousedown";
+                    break;
+                case "up":
+                    type_str = "mouseup";
+                    break;
+                case "move":
+                    type_str = "mousemove";
                     break;
                 default:
-                    console.log("Shouldn't get here (params.xtrs)")
+                    console.log("Shouldn't get here (params.evt)")
                     return null;
             }
+            // btn determination
+            switch (params.btn) {
+                case "lbtn":
+                    handleB = function(e) {
+                        if (e.button == 0) {
+                            return handle(e)
+                        }
+                    }
+                    break;
+                case "mbtn":
+                    handleB = function(e) {
+                        if (e.button == 1) {
+                            return handle(e)
+                        }
+                    }
+                    break;
+                default:
+                    // we can ignore buttons for the 'mousemove' event
+                    if (params.evt !== "move") {
+                        console.log("Shouldn't get here (params.btn)")
+                        return null;
+                    }
+                    handleB = handle
+            }
+
+
+
+        }
+        var bitvec = [false, false, false, false];
+        // add meta key handler
+        if (params.xtrs.length > 0) {
+            var does = params.xtrs;
+            for (var i = 0; i < does.length; i++) {
+                switch (does[i]) {
+                    case "alt":
+                        bitvec[0] = true;
+                        break;
+                    case "ctrl":
+                        bitvec[1] = true;
+                        break;
+                    case "meta":
+                        bitvec[2] = true;
+                        break;
+                    case "shift":
+                        bitvec[3] = true;
+                        break;
+                    default:
+                        console.log("Shouldn't get here (params.xtrs)")
+                        return null;
+                }
+            }
+
+            console.log(bitvec);
+            handleX = function(e) {
+                var m = e.originalEvent;
+                var pass = true;
+
+                pass = (pass && (bitvec[0] == e.altKey));
+                pass = (pass && (bitvec[1] == e.ctrlKey));
+                pass = (pass && (bitvec[2] == e.metaKey));
+                pass = (pass && (bitvec[3] == e.shiftKey));
+
+                if (pass) {
+                    return handleB(e);
+                }
+            }
+
+        } else {
+            handleX = handleB;
         }
 
-        console.log(bitvec);
-        handleX = function(e) {
-            var m = e.originalEvent;
-            var pass = true;
+        if (params.prevent == true) {
+            console.log("prevent registered");
+            handleP = function(e) {
+                e.preventDefault();
+                return handleX(e);
+            }
+        } else {
+            handleP = handleX;
+        }
 
-            pass = (pass && (bitvec[0] == e.altKey));
-            pass = (pass && (bitvec[1] == e.ctrlKey));
-            pass = (pass && (bitvec[2] == e.metaKey));
-            pass = (pass && (bitvec[3] == e.shiftKey));
+        var handleT = handleP
+        var canCall = true;
+        var limit = lmt;
 
-            if (pass) {
-                return handleB(e);
+        console.log(lmt);
+
+        if (lmt != undefined) {
+            handleT = function(e) {
+                if (!canCall)
+                    return;
+                canCall = false;
+                setTimeout(function() {
+                    canCall = true;
+                }, limit);
+                // call the actual handler
+                handleP(e);
             }
         }
-
-    } else {
-        handleX = handleB;
+        return {
+            type: type_str,
+            handler: handleT,
+        };
     }
 
-    if (params.prevent == true) {
-        console.log("prevent registered");
-        handleP = function(e) {
-            e.preventDefault();
-            return handleX(e);
+    var pass_test_strs = [
+        "click lbtn",
+        "double lbtn",
+        "click mbtn",
+        "double mbtn",
+        "click rbtn",
+        "double rbtn",
+        "move",
+        "move lbtn",
+        "move ctrl",
+
+        // space test
+        " double  rbtn ",
+
+    ];
+    var fail_test_strs = [
+
+        // invalid input
+        "xxx click rbtn",
+        "click xxx rbtn",
+        "click rbtn xxx",
+
+        // too many events
+        "click double rbtn",
+        "click rbtn double",
+        "rbtn click double",
+
+        // too many buttons
+        "double lbtn rbtn",
+        "lbtn double rbtn",
+        "lbtn rbtn double",
+
+        // and last but not least... the empty test!
+        "" // <- note the empty string
+    ];
+
+    function testParsing() {
+        console.log("Passing input:");
+        for (var i = 0; i < pass_test_strs.length; i++) {
+            console.log("test: " + i + "  " + pass_test_strs[i]);
+            var e = parseEvent(pass_test_strs[i], function() {});
+            if (e == null) {
+                console.log("[FAIL] ", e);
+            }
         }
-    } else {
-        handleP = handleX;
-    }
-
-
-
-    return {
-        type: type_str,
-        handler: handleP
-    };
-}
-
-function setHandles(jqry_sel, mm_handles) {
-
-}
-
-
-var pass_test_strs = [
-    "clk lbtn",
-    "dbl lbtn",
-    "clk mbtn",
-    "dbl mbtn",
-    "clk rbtn",
-    "dbl rbtn",
-
-    // space test
-    " dbl  rbtn ",
-
-];
-var fail_test_strs = [
-
-    // invalid input
-    "xxx clk rbtn",
-    "clk xxx rbtn",
-    "clk rbtn xxx",
-
-    // too many events
-    "clk dbl rbtn",
-    "clk rbtn dbl",
-    "rbtn clk dbl",
-
-    // too many buttons
-    "dbl lbtn rbtn",
-    "lbtn dbl rbtn",
-    "lbtn rbtn dbl",
-
-    // and last but not least... the empty test!
-    "" // <- note the empty string
-];
-
-mightymouse.Test = function() {
-    console.log("Passing input:");
-    for (var i = 0; i < pass_test_strs.length; i++) {
-        console.log("test: " + i + "  " + pass_test_strs[i]);
-        var e = parseEvent(pass_test_strs[i], function() {});
-        if (e == null) {
-            console.log("[FAIL] ", e);
+        console.log("Failing input:");
+        for (var i = 0; i < fail_test_strs.length; i++) {
+            console.log("test: " + i + "  " + fail_test_strs[i]);
+            var e = parseEvent(fail_test_strs[i], function() {});
+            if (e != null) {
+                console.log("[FAIL] ", e);
+            }
         }
     }
-    console.log("Failing input:");
-    for (var i = 0; i < fail_test_strs.length; i++) {
-        console.log("test: " + i + "  " + fail_test_strs[i]);
-        var e = parseEvent(fail_test_strs[i], function() {});
-        if (e != null) {
-            console.log("[FAIL] ", e);
-        }
+
+
+    MM.TestMices = function() {
+        testParsing();
+
+        MM.AddHandler("body", "rbtn prevent", function(e) {
+            console.log("right mouse prevented!");
+        });
+        MM.AddHandler("body", "lbtn click ctrl", function(e) {
+            console.log("mighty mouse to the rescue!")
+        });
+        MM.AddHandler("body", "lbtn click shift", function(e) {
+            console.log("mighty mouse to the rescue2!")
+        });
+        MM.AddHandler("body", "lbtn click ctrl shift", function(e) {
+            console.log("UP UP AND AWAY!");
+        });
+
+        var rateLimit = 200;
+        MM.AddHandler("body", "move", function(e) {
+            console.log("MM pos: " + e.clientX + "," + e.clientY + " @ " + new(Date))
+        }, rateLimit);
     }
-}
 
 
-function TestMices() {
-    mightymouse.AddHandler("body", "rbtn prevent", function(e) {
-        console.log("right mouse prevented!");
-    });
+    return MM;
 
-    mightymouse.AddHandler("body", "lbtn clk ctrl", function(e) {
-        console.log("mighty mouse to the rescue!")
-    });
-    mightymouse.AddHandler("body", "lbtn clk shift", function(e) {
-        console.log("mighty mouse to the rescue2!")
-    });
-    mightymouse.AddHandler("body", "lbtn clk ctrl shift", function(e) {
-        console.log("UP UP AND AWAY!");
-    });
-
-}
+}(jQuery));
